48b27668eff3a82f6e521bc00ad89a1b17ef60a6
polinasevko <polinasevko24@gmail.com>
2022-09-03 15:05:13 +0300

    Add field of performer to concert model

--- concert/models.py
+++ concert/models.py
@@ -6,24 +6,37 @@ class ConcertType(models.Model):
     name = models.CharField(max_length=20)
     slug = models.SlugField(unique=True)
 
+    def __str__(self) -> str:
+        return f'Type {self.name}'
+
 
 class Concert(models.Model):
     name = models.CharField(max_length=20)
+    performer  = models.CharField(max_length=20)
     type = models.ForeignKey(ConcertType, on_delete=models.CASCADE)
     tickets_number = models.PositiveIntegerField(default=0)
     date = models.DateTimeField()
     address = models.TextField(max_length=100)
     price = models.DecimalField(max_digits=12, decimal_places=2)
     description = models.TextField(max_length=200, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f'Concert {self.name}'
     
 
 class TypeCharacteristic(models.Model):
     concert_type = models.ForeignKey(ConcertType, on_delete=models.CASCADE)
     name = name = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return f'Type characteristic {self.name}'
+
 
 class ValueOfCharacteristic(models.Model):
     type_characteristic = models.ForeignKey(TypeCharacteristic, on_delete=models.CASCADE)
     value = models.CharField(max_length=20)
     concerts = models.ManyToManyField(Concert, blank=True, related_name='characteristics')
+
+    def __str__(self) -> str:
+        return f'Characteristic value {self.value}'
     
