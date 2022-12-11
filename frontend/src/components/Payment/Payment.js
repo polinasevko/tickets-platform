565091502430be951d36bbf58d6fcae5cb02cbc4
polinasevko <polinasevko24@gmail.com>
2022-12-12 01:38:00 +0300

    cleaning

--- frontend/src/components/Payment/Payment.js
+++ frontend/src/components/Payment/Payment.js
@@ -2,8 +2,6 @@ import React from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const Payment = ({ price, handleOrder, orderId }) => {
-  console.log(orderId);
-  console.log(price);
   return (
     <PayPalScriptProvider
       options={{ "client-id": process.env.REACT_APP_PAYPAL_CLIENT_ID }}
@@ -23,8 +21,6 @@ const Payment = ({ price, handleOrder, orderId }) => {
           });
         }}
         onApprove={(data, actions) => {
-          // let response = actions.order.capture().then();
-          // return handleOrder(response);
           return actions.order.capture().then(handleOrder);
         }}
         onCancel={() => alert("You cancelled payment.")}
